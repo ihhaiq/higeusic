58f7e4b8a125469af7e411399c37c7a69c6aebfb
HUSSEIN <byhusseins@gmail.com>
2026-09-12 01:56:27 +0300

    Support channel play logs without from_user

--- AlexaMusic/utils/logger.py
+++ AlexaMusic/utils/logger.py
@@ -25,9 +25,14 @@ async def play_logs(message, streamtype):
 <b>𝖢𝗁𝖺𝗍 𝖭𝖺𝗆𝖾 :</b> {message.chat.title}
 <b>𝖢𝗁𝖺𝗍 𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.chat.username}
 
-<b>𝖴𝗌𝖾𝗋 𝖨𝖣 :</b> <code>{message.from_user.id}</code>
-<b>𝖴𝗌𝖾𝗋 𝖭𝖺𝗆𝖾 :</b> {message.from_user.mention}
-<b>𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.from_user.username}
+<b>𝖱𝖾𝗊𝗎𝖾𝗌𝗍𝖾𝗋 :</b> {
+    message.from_user.mention
+    if message.from_user
+    else (message.author_signature or message.chat.title or "Channel post")
+}
+<b>𝖱𝖾𝗊𝗎𝖾𝗌𝗍𝖾𝗋 𝖨𝖣 :</b> <code>{
+    message.from_user.id if message.from_user else message.chat.id
+}</code>
 
 <b>𝖰𝗎𝖾𝗋𝗒 :</b> {message.text.split(None, 1)[1]}
 <b>𝖲𝗍𝗋𝖾𝖺𝗆-𝖳𝗒𝗉𝖾 :</b> {streamtype}"""
