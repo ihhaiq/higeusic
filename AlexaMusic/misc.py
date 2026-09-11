33440496cc788fdc5e0f4df17390a67d6540f5e2
HUSSEIN <byhusseins@gmail.com>
2026-09-12 02:25:36 +0300

    Remove unused Heroku runtime code

--- AlexaMusic/misc.py
+++ AlexaMusic/misc.py
@@ -1,18 +1,5 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-import socket
 import time
 
-import heroku3
 from pyrogram import filters
 
 import config
@@ -21,30 +8,7 @@ from AlexaMusic.core.mongo import mongodb
 from .logging import LOGGER
 
 SUDOERS = filters.user()
-
-HAPP = None
 _boot_ = time.time()
-
-
-def is_heroku():
-    return "heroku" in socket.getfqdn()
-
-
-XCB = [
-    "/",
-    "@",
-    ".",
-    "com",
-    ":",
-    "git",
-    "heroku",
-    "push",
-    str(config.HEROKU_API_KEY),
-    "https",
-    str(config.HEROKU_APP_NAME),
-    "HEAD",
-    "master",
-]
 
 
 def dbb():
@@ -66,20 +30,6 @@ async def sudo():
             {"$set": {"sudoers": sudoers}},
             upsert=True,
         )
-    if sudoers:
-        for user_id in sudoers:
-            SUDOERS.add(user_id)
+    for user_id in sudoers:
+        SUDOERS.add(user_id)
     LOGGER(__name__).info("Sudoers Loaded.")
-
-
-def heroku():
-    global HAPP
-    if is_heroku and (config.HEROKU_API_KEY and config.HEROKU_APP_NAME):
-        try:
-            Heroku = heroku3.from_key(config.HEROKU_API_KEY)
-            HAPP = Heroku.app(config.HEROKU_APP_NAME)
-            LOGGER(__name__).info("Heroku App Configured")
-        except BaseException:
-            LOGGER(__name__).warning(
-                f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
-            )
