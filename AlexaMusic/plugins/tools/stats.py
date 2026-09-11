445f42412ea9d6d87b077ce54c4d80f134ad3e04
HUSSEIN <byhusseins@gmail.com>
2026-09-12 02:24:54 +0300

    Remove legacy developer channel from stats

--- AlexaMusic/plugins/tools/stats.py
+++ AlexaMusic/plugins/tools/stats.py
@@ -177,7 +177,7 @@ async def top_users_ten(client, CallbackQuery: CallbackQuery, _):
                 details = stats.get(items)
                 title = (details["title"][:35]).title()
                 if items == "telegram":
-                    msg += f"🌹 [ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/Shayri_Music_Lovers) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
+                    msg += f"🌹 **وسائط تيليجرام** — تم تشغيلها {count} مرة\n\n"
                 else:
                     msg += f"🌹 [{title}](https://www.youtube.com/watch?v={items}) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
 
