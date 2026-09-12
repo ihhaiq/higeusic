dbf1fb7ad2434e11c125a2060194acff9d6ed4db
HUSSEIN <byhusseins@gmail.com>
2026-09-12 04:53:29 +0300

    Use rich card for automatic SoundCloud playback

--- AlexaMusic/core/call.py
+++ AlexaMusic/core/call.py
@@ -520,15 +520,17 @@ class Call(PyTgCalls):
                     db[chat_id][0]["mystic"] = run
                     db[chat_id][0]["markup"] = "rich"
                 elif videoid == "soundcloud":
-                    button = telegram_markup(_, chat_id)
-                    run = await app.send_photo(
+                    requester_id = check[0].get("user_id") or config.OWNER_ID
+                    run = await send_stream_rich_message(
                         original_chat_id,
-                        photo=config.SOUNCLOUD_IMG_URL,
-                        caption=_["stream_3"].format(title, check[0]["dur"], user),
-                        reply_markup=InlineKeyboardMarkup(button),
+                        image=config.SOUNCLOUD_IMG_URL,
+                        title=title,
+                        is_video=False,
+                        requester_id=requester_id,
+                        duration=check[0]["dur"],
                     )
                     db[chat_id][0]["mystic"] = run
-                    db[chat_id][0]["markup"] = "tg"
+                    db[chat_id][0]["markup"] = "rich"
                 else:
                     # theme = await check_theme(chat_id)
                     img = await gen_thumb(videoid)
