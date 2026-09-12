509c835c69adcad52568d31fc11730619c5b14e8
HUSSEIN <byhusseins@gmail.com>
2026-09-12 10:54:55 +0300

    Accept plain Arabic video playback syntax

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -43,7 +43,7 @@ from AlexaMusic.utils.database import is_served_user
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
-@app.on_message(filters.command(PLAY_COMMAND) & (filters.group | filters.channel) & ~BANNED_USERS)
+@app.on_message((filters.command(PLAY_COMMAND) | filters.command(["فيديو"], prefixes="")) & (filters.group | filters.channel) & ~BANNED_USERS)
 @PlayWrapper
 async def play_commnd(
     client,
